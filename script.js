98e1519bb3a11ab3138d79920f4dd933e242119a
Maksym <borovskyimaksym@gmail.com>
2023-07-06 21:33:47 +0300

    old

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-window.onload = function() {
+
+window.onload = function () {
     document.addEventListener('click', documentActions);
 }
 
-function documentActions(e) {
+function documentActions (e) {
     let targetElement = e.target;
-    if (targetElement.classList.contains('icon-menu')) {
+    if(targetElement.classList.contains('icon-menu')){
         targetElement.closest('.header__main').classList.toggle('active-menu');
         targetElement.classList.toggle('active');
         document.body.classList.toggle('lock');
