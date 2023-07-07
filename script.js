e07647f1b665136c595e9588e44db46c2bdf1c04
Maksym <borovskyimaksym@gmail.com>
2023-07-07 21:08:07 +0300

    m

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-
 window.onload = function () {
-    document.addEventListener('click', documentActions);
-    $('.slider-reviews__body').on('init', function(event, slick){
-        console.log("initialized")
-    });
-    $('.slider-reviews__body').slick({
-        infinite: true,
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '.slider-arrow_prev',
-        nextArrow: '.slider-arrow_next',
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                autoplay: true,
-              }
-            }
-          ]
-      });
-}
-
-function documentActions (e) {
-    let targetElement = e.target;
-    if(targetElement.classList.contains('icon-menu')){
-        targetElement.closest('.header__main').classList.toggle('active-menu');
-        targetElement.classList.toggle('active');
-        document.body.classList.toggle('lock');
+    document.addEventListener("click", documentActions)
+    $(".slider-reviews__body").on("init", function (event, slick) {
+      console.log("initialized")
+    })
+    $(".slider-reviews__body").slick({
+      infinite: true,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: ".slider-arrow_prev",
+      nextArrow: ".slider-arrow_next",
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            autoplay: true,
+          },
+        },
+      ],
+    })
+  }
+  
+  function documentActions(e) {
+    let targetElement = e.target
+    if (targetElement.classList.contains("icon-menu")) {
+      targetElement.closest(".header__main").classList.toggle("active-menu")
+      targetElement.classList.toggle("active")
+      document.body.classList.toggle("lock")
     }
-}
+  }
+  
