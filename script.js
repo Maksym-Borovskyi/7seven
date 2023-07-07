a04a33be0c5699c1c031fc09f973d3fa064aa131
Maksym <borovskyimaksym@gmail.com>
2023-07-07 21:46:34 +0300

    phone

--- script.js
+++ script.js
@@ -19,6 +19,22 @@ window.onload = function () {
         },
       ],
     })
+
+    var phone_input = document.getElementById("phone");
+
+phone_input.addEventListener('input', () => {
+  phone_input.setCustomValidity('');
+  phone_input.checkValidity();
+});
+
+phone_input.addEventListener('invalid', () => {
+  if(phone_input.value === '') {
+    phone_input.setCustomValidity('Enter phone number!');
+  } else {
+    phone_input.setCustomValidity('Enter phone number in this format: 0991112222');
+  }
+});
+
   }
   
   function documentActions(e) {
